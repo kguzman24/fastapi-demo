c9e335c86d6fa1689b5ababb22582d804e131079
Karen Guzman <krngzmn10@gmail.com>
2024-11-27 12:04:52 -0500

    genres and songs endpoints added

--- app/main.py
+++ app/main.py
@@ -1,29 +1,78 @@
 #!/usr/bin/env python3
 
+import mysql.connector
+from mysql.connector import Error
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
 import json
 import os
 
-app = FastAPI()
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "ds2022"
+DBPASS = os.getenv('DBPASS')
+DB = "sae3gg"
 
+app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/genres')
+def get_genres():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+    cur=db.cursor()
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
+        return(json_data)
+    except Error as e:
+        cur.close()
+        db.close()
+        return {"Error": "MySQL Error: " + str(e)}
+    
+@app.get('/songs')
+def get_songs():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+    cur=db.cursor()
+    query = """
+        SELECT 
+            songs.title,
+            songs.album,
+            songs.artist,
+            genres.genre AS genre,
+            songs.year,
+            songs.file,
+            songs.image
+        FROM songs
+        JOIN genres ON songs.genre = genres.genreid
+        ORDER BY songs.id;
+    """
+    try:
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
+        return(json_data)
+    except Error as e:
+        cur.close()
+        db.close()
+        return {"Error": "MySQL Error: " + str(e)}
 @app.get("/")  # zone apex
 def zone_apex():
-    return {"Bye": "Hello API"}
-
-@app.get("/add/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
-
-@app.get("/square/{a}")
-def square(a: int):
-    return {"Squared product": a*a}
-
-@app.get("/data")
-def data():
-    return {"Man I love data"}
-
-@app.get("/do_nothing")
-def nothing():
-    return {" "}
+    return {"This is just a blank page silly"}
